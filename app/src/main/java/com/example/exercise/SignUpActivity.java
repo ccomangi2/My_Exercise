57b3e25a640ee326e5053d7d11c9b8e09f58434f
Lee Su Bin <ccomangi2@gmail.com>
2021-05-29 10:25:51 +0900

    매니페스트에 액티비티 등록

--- app/src/main/java/com/example/exercise/SignUpActivity.java
+++ app/src/main/java/com/example/exercise/SignUpActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignUpActivitiy extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
